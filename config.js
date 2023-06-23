81b753ad2e833a7aa41a1f3ad8bf2bebf87ad899
Andrew <ondralukes06@seznam.cz>
2023-06-23 19:50:22 +0200

    coppied my server app

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC1tsEH0k0ZQ6iUHUOxjAsA6ypYOQh6qsU",
+    authDomain: "fileshraingapp.firebaseapp.com",
+    projectId: "fileshraingapp",
+    storageBucket: "fileshraingapp.appspot.com",
+    messagingSenderId: "973775377545",
+    appId: "1:973775377545:web:53747856538339a639e258"
+  };
---
+++ server.js
@@ -0,0 +1,231 @@
+ 
+ 
+
+const express = require("express");
+const multer = require('multer');
+const admin = require("firebase-admin");
+const { v4: uuidv4 } = require('uuid');
+const path = require('path');
+const { getDownloadURL, ref } = require('firebase/storage');
+var serviceAccount = require("./config.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: "fileshraingapp.appspot.com"
+  });
+ 
+const app = express();
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+const db = admin.firestore();
+const filesCollection = db.collection("files");
+const storageRef = admin.storage().bucket();
+
+const bucket = admin.storage().bucket();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/api", (req,res)=> {
+    res.json({"users": [ "userone", "usertwo", "userthree" ]})
+})
+ 
+ 
+app.get("/gallery", async (req, res) => {
+  try {
+    const files = [];
+    const [filesResponse] = await storageRef.getFiles({ prefix: 'files/' });
+
+    if (filesResponse[0].name === "files/") {
+      filesResponse.shift(); // Remove the first file
+    }
+
+    for (const file of filesResponse) {
+      const [metadata] = await file.getMetadata();
+      const downloadURL = await file.getSignedUrl({
+        action: 'read',
+        expires: '03-01-2500' // Adjust the expiry date as needed
+      });
+
+      console.log(metadata.name, downloadURL[0]);
+
+      files.push({
+        filename: metadata.name,
+        downloadURL: downloadURL[0]
+      });
+    }
+
+    // Log the files to the console
+    console.log("Files:");
+    files.forEach(file => {
+      console.log(file);
+    });
+
+    // Generate the HTML content dynamically
+    
+
+    res.send(files); // Send the dynamically generated HTML content as the response
+  } catch (error) {
+    console.error("Error fetching image URLs:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+
+app.post('/add', upload.single('file'), async (req, res) => {
+    try {
+      const { filename, user } = req.body;
+      const file = req.file;
+  
+      // Check if a file with the same filename already exists
+      const existingFiles = await bucket.getFiles({
+        prefix: `files/${filename}`
+      });
+  
+      if (existingFiles[0].length > 0) {
+        return res.status(400).send('File with the same filename already exists');
+      }
+  
+      // Generate a unique filename using UUID
+      const uniqueFilename = `${uuidv4()}_${filename}_${user}`;
+  
+      // Upload the file to Firebase Storage
+      const blob = bucket.file(`files/${uniqueFilename}`);
+      const blobStream = blob.createWriteStream();
+  
+      blobStream.on('error', (err) => {
+        console.error('Error uploading file:', err);
+        return res.status(500).send('Internal Server Error');
+      });
+  
+      blobStream.on('finish', async () => {
+        // Get the download URL of the uploaded file
+        const downloadURL = await blob.getSignedUrl({
+          action: 'read',
+          expires: '03-01-2500' // Adjust the expiry date as needed
+        });
+  
+        // Save the file details in Firestore or perform any other required operations
+        // ...
+        
+        return res.status(200).send(`File added successfully with URL: ${downloadURL}`);
+      });
+  
+      blobStream.end(file.buffer);
+    } catch (error) {
+      console.error('Error adding file:', error);
+      return res.status(500).send('Internal Server Error');
+    }
+  });
+
+  async function listFiles() {
+    try {
+      const [files] = await admin.storage().bucket().getFiles({
+        prefix: "files/"
+      });
+  
+      console.log("Files in the folder:");
+      files.forEach(file => {
+        console.log(file.name);
+      });
+    } catch (error) {
+      console.error("Error listing files:", error);
+    }
+  }
+  
+  // Call the function to list files
+  listFiles();
+  
+
+  // app.get('/', async (req, res) => {
+//     try {
+//         let response = [];
+
+//         const querySnapshot = await filesCollection.get();
+//         querySnapshot.forEach((doc) => {
+//             response.push(doc.data());
+//         });
+
+//         // Render the "index.ejs" file and pass the response data to it
+//         res.render('index', { files: response });
+//     } catch (error) {
+//         res.status(500).send(error);
+//     }
+// });
+
+ 
+  
+// Serve static files from the "public" directory
+ 
+// app.get("/", async (req, res) => {
+//   try {
+//     let response = [];
+
+//     const querySnapshot = await filesCollection.get();
+//     querySnapshot.forEach((doc) => {
+//       response.push(doc.data());
+//     });
+//     return  res.sendFile('index.html', { root: './public' });
+//     // return res.status(200).send(response);
+//   } catch (error) {
+//     return res.status(500).send(error);
+//   }
+// });
+
+  app.put('/update', async (req, res) => {
+    try {
+      const { user, filename, contents } = req.body;
+  
+      // Query the collection to find the file with matching username and filename
+      const querySnapshot = await filesCollection
+        .where('user', '==', user)
+        .where('filename', '==', filename)
+        .get();
+  
+      if (querySnapshot.empty) {
+        return res.status(404).send('File not found');
+      }
+  
+      // Update the contents of the first matching document
+      const fileDoc = querySnapshot.docs[0];
+      await fileDoc.ref.update({ contents });
+  
+      return res.status(200).send('File updated successfully');
+    } catch (error) {
+      console.error('Error updating file:', error);
+      return res.status(500).send('Internal Server Error');
+    }
+  });
+
+  app.delete('/delete', async (req, res) => {
+    try {
+      const { user, filename } = req.body;
+      console.log(user, filename);
+  
+      // Find the document(s) matching the provided username and filename
+      const querySnapshot = await filesCollection
+        .where('user', '==', user)
+        .where('filename', '==', filename)
+        .get();
+  
+      // Check if any matching document(s) were found
+      if (querySnapshot.empty) {
+        return res.status(404).send('File not found');
+      }
+  
+      // Delete the matching document(s)
+      const deletePromises = querySnapshot.docs.map((doc) => doc.ref.delete());
+      await Promise.all(deletePromises);
+  
+      return res.status(200).send('File deleted successfully');
+    } catch (error) {
+      console.error('Error deleting file:', error);
+      return res.status(500).send('Internal Server Error');
+    }
+  });
+  
+   const PORT = 5000;//dont change this
+
+   app.listen(PORT, ()=>{
+    console.log(`Server is runnning on port https//localhost:${PORT}`)
+   })
